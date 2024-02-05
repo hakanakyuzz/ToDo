4ed2a4f3725c872b5e42fef57fbe3e86c76470ed
Hakan <legion61614@gmail.com>
2024-02-06 01:35:55 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const input = document.querySelector("input")
+const button = document.querySelector("button")
+const ul = document.querySelector("ul")
+
+button.addEventListener("click", () => {
+    if (input.value === "")
+        alert("Empty")
+    else{
+        let li = document.createElement("li")
+        li.innerHTML = input.value
+        ul.appendChild(li)
+
+        let span = document.createElement("span")
+        span.innerHTML = "X"
+        li.appendChild(span)
+    }
+    input.value = ""
+    saveData()
+})
+
+ul.addEventListener("click", (e) => {
+    if (e.target.tagName === "LI")
+        e.target.classList.toggle("checked")
+    else if(e.target.tagName === "SPAN")
+        e.target.parentElement.remove()
+    saveData()
+})
+
+function saveData() {
+    localStorage.setItem("data", ul.innerHTML)
+}
+
+function showData() {
+    ul.innerHTML = localStorage.getItem("data")
+}
+showData()
